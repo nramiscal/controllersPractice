a530e9a202b6aa86a48695516dcbc66148447c43
nramiscal <nramiscal@gmail.com>
2017-09-10 17:00:42 -0400

    first commit

---
+++ src/test/java/com/nramiscal/controllersPractice/ControllersPracticeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nramiscal.controllersPractice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ControllersPracticeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
